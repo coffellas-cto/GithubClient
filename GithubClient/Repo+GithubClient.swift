0baf312c7156d2f96138845876b898dabadf3d36
coffellas-cto <cto@coffellas.com>
2014-10-22 16:09:02 -0700

    Updated repoFromNSDictionary method to avoid crazy crash when working with 2126244 number

--- GithubClient/Repo+GithubClient.swift
+++ GithubClient/Repo+GithubClient.swift
@@ -21,38 +21,49 @@ extension Repo {
     }
     
     class func repoFromNSDictionary(dic: NSDictionary) -> Repo? {
-        let id = dic["id"] as Int!
-        if id == nil {
-            return nil
-        }
         
-        var retVal: Repo!
-        var mustUpdate = true
+        // "Magic" numbers here
+        // Some crazy stuff happens with 2126244 number (app crashes for no reason), ignore that value!
+        // We need all that convertion to string and back, so we can access the actual value
         
-        let resultsArray = CoreDataManager.manager.fetchObjectsWithEntityClass(Repo.classForCoder(), predicateFormat: "id == %@", NSNumber(integer: id))
-        if (resultsArray != nil) && (resultsArray?.count > 0) {
-            retVal = resultsArray!.first as? Repo
-            if let updateDate = dic["updated_at"] as String? {
-                mustUpdate = retVal.updateDate != updateDate
+        if let idAny: AnyObject? = dic["id"] {
+            let idString: NSString = "\(idAny!)"
+            if let id = idString.integerValue as NSNumber? {
+                if id.isEqualToValue(2126244) {
+                    return nil
+                }
+                
+                var retVal: Repo!
+                var mustUpdate = true
+                
+                let resultsArray = CoreDataManager.manager.fetchObjectsWithEntityClass(Repo.classForCoder(), predicateFormat: "id == %@", id)
+                if (resultsArray != nil) && (resultsArray?.count > 0) {
+                    retVal = resultsArray!.first as? Repo
+                    if let updateDate = dic["updated_at"] as String? {
+                        mustUpdate = retVal.updateDate != updateDate
+                    }
+                }
+                else {
+                    retVal = CoreDataManager.manager.newObjectForEntityClass(Repo) as? Repo
+                }
+                
+                if mustUpdate && retVal != nil {
+                    retVal.id = Int64(id.integerValue)
+                    retVal.name = dic["name"] as String
+                    retVal.fullName = dic["full_name"] as String
+                    retVal.htmlUrl = dic["html_url"] as String
+                    retVal.isPrivate = dic["private"] as Bool
+                    retVal.language = dic["language"] as? String
+                    retVal.stargazersCount = Int16(dic["stargazers_count"] as Int)
+                    retVal.updateDate = dic["updated_at"] as String
+                    retVal.descriptionString = dic["description"] as? String
+                    retVal.user = User.userFromNSDictionary(dic["owner"] as NSDictionary)
+                }
+                
+                return retVal
             }
         }
-        else {
-            retVal = CoreDataManager.manager.newObjectForEntityClass(Repo) as? Repo
-        }
         
-        if mustUpdate && retVal != nil {
-            retVal.id = Int64(id)
-            retVal.name = dic["name"] as String
-            retVal.fullName = dic["full_name"] as String
-            retVal.htmlUrl = dic["html_url"] as String
-            retVal.isPrivate = dic["private"] as Bool
-            retVal.language = dic["language"] as? String
-            retVal.stargazersCount = Int16(dic["stargazers_count"] as Int)
-            retVal.updateDate = dic["updated_at"] as String
-            retVal.descriptionString = dic["description"] as? String
-            retVal.user = User.userFromNSDictionary(dic["owner"] as NSDictionary)
-        }
-        
-        return retVal
+        return nil
     }
 }
